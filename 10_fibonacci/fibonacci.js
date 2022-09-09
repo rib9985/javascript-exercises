893490263f67b9df19241b9b27a58e3321b1582c
rib9985 <rib9985@gmail.com>
2022-09-08 23:38:26 -0300

    Add solution and remove test skips

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+const finalIndex = parseInt(number)
+if (finalIndex===0){
+    return 0
+}
+else if (finalIndex<0){
+    return "OOPS"
+}
+else{
+    let fiboArray= [1,1]
+    for(let count= 1;count<finalIndex-1;count++){ 
+        fiboArray.push((fiboArray[count]+fiboArray[(count-1)]))           
+        }
+        return fiboArray[finalIndex-1]
+    }
+    
+}
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
