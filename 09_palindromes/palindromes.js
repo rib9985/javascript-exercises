78539fe4c267d234818baa4caea4c11f7ea58bea
rib9985 <rib9985@gmail.com>
2022-09-08 21:55:23 -0300

    Add solution and edit test skips in spec

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const originalString = string
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g
+let noCaps = originalString.toLowerCase()
+let finalString = noCaps.replace(regex,"")
+let reverseString = finalString.split("").reverse().join("");
+
+return finalString===reverseString ? true:false
+
 
 };
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 
 // Do not edit below this line
 module.exports = palindromes;
