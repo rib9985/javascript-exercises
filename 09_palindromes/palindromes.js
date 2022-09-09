f95d6e858aaaee43c4287b346192c7671ce6e7f7
rib9985 <rib9985@gmail.com>
2022-09-08 22:45:34 -0300

    Refactor for better chaining and readability

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,21 @@
 const palindromes = function (string) {
 const originalString = string
 const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g
-let noCaps = originalString.toLowerCase()
-let finalString = noCaps.replace(regex,"")
-let reverseString = finalString.split("").reverse().join("");
 
-return finalString===reverseString ? true:false
+let processedString = 
+originalString
+.toLowerCase()
+.replace(regex,"");
+
+let reverseString = processedString
+.split("")
+.reverse()
+.join("");
+
+return processedString===reverseString ? true:false
 
 
 };
-console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 
 // Do not edit below this line
 module.exports = palindromes;
